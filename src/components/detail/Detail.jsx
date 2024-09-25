4efc5d496ed9fc10f441b750c13594b1744138a1
Preeti Chauhan <preeti.chauhan@rbhsolutions.com>
2024-09-25 13:50:42 +0530

    details

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,104 @@ import React from "react";
 import "./detail.css";
 
 function Detail() {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit consectetur.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy % help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxeAFDJREq0AMzWgajJLLhegrgaUEzBDyYeA&s"
+                  alt=""
+                />
+                <span>photo-2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxeAFDJREq0AMzWgajJLLhegrgaUEzBDyYeA&s"
+                  alt=""
+                />
+                <span>photo-2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxeAFDJREq0AMzWgajJLLhegrgaUEzBDyYeA&s"
+                  alt=""
+                />
+                <span>photo-2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxeAFDJREq0AMzWgajJLLhegrgaUEzBDyYeA&s"
+                  alt=""
+                />
+                <span>photo-2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxeAFDJREq0AMzWgajJLLhegrgaUEzBDyYeA&s"
+                  alt=""
+                />
+                <span>photo-2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxeAFDJREq0AMzWgajJLLhegrgaUEzBDyYeA&s"
+                  alt=""
+                />
+                <span>photo-2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
